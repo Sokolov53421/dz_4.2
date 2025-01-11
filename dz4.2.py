c218596dcda6118d542b278650c1bc5321d6c94b
Sokolov53421 <isokolov55530@gmail.com>
2025-01-11 18:17:30 +0200

    dz 4.2

--- dz4.2.py
+++ dz4.2.py
@@ -1,8 +1,8 @@
 my_list = [3, 4, 5, 2, 2, 7, 4]
 if my_list:
-    list_1 = sum(my_list[i] for i in range(0, len(my_list), 2))
-    elements_list = my_list[-1]
-    result = list_1 * elements_list
+    sum_element = sum(my_list[i] for i in range(0, len(my_list), 2))
+    last_elements_list = my_list[-1]
+    result = sum_element * last_elements_list
 else:
     result = 0
 print(result)
